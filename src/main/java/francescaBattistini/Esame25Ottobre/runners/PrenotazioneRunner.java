e72d6edc8e936d32c8d9c02334dc2eed96b1a900
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-25 17:04:53 +0200

    Update PrenotazioneRunner.java

--- src/main/java/francescaBattistini/Esame25Ottobre/runners/PrenotazioneRunner.java
+++ src/main/java/francescaBattistini/Esame25Ottobre/runners/PrenotazioneRunner.java
@@ -1,8 +1,5 @@
 package francescaBattistini.Esame25Ottobre.runners;
 
-import francescaBattistini.Esame25Ottobre.entities.Postazione;
-import francescaBattistini.Esame25Ottobre.entities.Prenotazione;
-import francescaBattistini.Esame25Ottobre.entities.Utente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
