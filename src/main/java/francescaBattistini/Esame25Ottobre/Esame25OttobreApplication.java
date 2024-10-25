21cef7a696ca8c52d1bae9d5e8fff7a4a431d17d
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-25 11:13:26 +0200

    Initial commit

---
+++ src/main/java/francescaBattistini/Esame25Ottobre/Esame25OttobreApplication.java
@@ -0,0 +1,13 @@
+package francescaBattistini.Esame25Ottobre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esame25OttobreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esame25OttobreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescaBattistini/Esame25Ottobre/Esame25OttobreApplicationTests.java
@@ -0,0 +1,13 @@
+package francescaBattistini.Esame25Ottobre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esame25OttobreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
